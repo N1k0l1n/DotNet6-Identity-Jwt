cab4c605a884e0c664aba21063d08e5ea0904e87
N1k0L1n <nikolinandoni80@gmail.com>
2023-12-21 13:52:23 +0100

    ProtectedController

---
+++ Controllers/ProtectedController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApisBack.DTOs;
+
+namespace MoviesApisBack.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        public IActionResult GetData()
+        {
+            var status = new Status();
+            status.StatusCode = 1;
+            status.Message = "Data from protected controller";
+            return Ok(status);
+        }
+    }
+}
